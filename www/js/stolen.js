18e3436bf51319a05204fc5ccd265335d7eef531
Christopher Ishida <cishida@ucla.edu>
2014-03-04 03:15:52 -0800

    Added basic vehicles-stolen page functionality

---
+++ www/js/stolen.js
@@ -0,0 +1,16 @@
+function stolenInitialize() {
+  var vehicle = new Vehicle();
+
+  vehicle.getStolenVehicleList(function(vehicleArray) {
+    for (var i = 0; i < vehicleArray.length; i++) {
+
+      var vehicle = vehicleArray[i];
+      var vehicleText = vehicle.year + ' ' + vehicle.make + ' ' + vehicle.model;
+      var vehicleHTML = '<div class="list-item" onclick="setVehiclePage(1)">'
+                        + vehicleText + '</div>';
+
+      $('#stolen-list').append(vehicleHTML);
+    }
+  });
+}
+

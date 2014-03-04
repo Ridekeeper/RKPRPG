560e31e184888d75f425c530d2c4c2ea32405166
Christopher Ishida <cishida@ucla.edu>
2014-03-04 15:39:17 -0800

    Registration goes to Login

--- www/js/registration.js
+++ www/js/registration.js
@@ -2,11 +2,12 @@ function registration() {
 
   this.initialize = function() {
     // Set the highlighted page because we have no sidebar
-    Ridekeeper.highlightedPage = 'registration';
+    //Ridekeeper.highlightedPage = 'registration';
     // Register sign in callback
     //$('#registration-button').click(function(){Ridekeeper.registration.register()});
+    //alert("hi");
     // Register registration callback
-    $('#register-button').click(function(){Ridekeeper.registration.goToLogin()});
+    $('#registration-button').click(function(){Ridekeeper.registration.goToLogin()});
   }
 
   this.register = function() {

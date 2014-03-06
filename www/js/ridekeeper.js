3ff4a0e26b68834fdf86eaf8730ca5c4e446d234
Matthew McKeen <matthew@mmckeen.net>
2014-03-06 11:54:16 -0800

    Push notifications +1

--- www/js/ridekeeper.js
+++ www/js/ridekeeper.js
@@ -19,6 +19,24 @@ function ridekeeper() {
   // as soon as the Ridekeeper object is
   this.settings.initialize();
 
+  // register for push notifications
+  {
+    var clientKey = "obFmxyzaxx6JWMVMtXpaggDFu2TcARRMqyFQdYpP";
+    var appId     = "TfBH3NJxzbOaxpksu5YymD4lP9bPlytcfZMG8i5a";  
+
+    parsePlugin.initialize(appId, clientKey, function() {
+      alert('success');
+    }, function(e) {
+      alert('error');
+    });
+
+    parsePlugin.subscribe('phonegap', function() {
+        alert('OK');
+    }, function(e) {
+        alert('error');
+    });
+  }
+
   this.highlightedPage = "vehicles-stolen";
 }
 

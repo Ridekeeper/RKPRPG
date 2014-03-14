8fa288506366df588b84c1d9962e9fd0ac11773d
reed <readuw@gmail.com>
2014-03-13 23:17:49 -0700

    Vehicle pictures can be set

--- www/js/vehicle_info.js
+++ www/js/vehicle_info.js
@@ -26,7 +26,13 @@ function vehicleInfoInitialize() {
     } else {
       mapInitialize(vehicleObject.location);
     }
+    document.getElementById("vehicle-image").src = vehicleObject.photo;
   }, errorFun);
+
+  $('#change-vehicle-image-button').click(function(){
+    Ridekeeper.vehicles.newPicture();
+  });
+
 
   $('#back').click(function() {
     if (vehicleStolen) {
@@ -100,4 +106,92 @@ function setVehiclePage(vehicleIdentifier, stolen) {
   pageVehicleId = vehicleIdentifier;
   vehicleStolen = stolen;
   window.open("#/vehicle-map", "_self");
+}
+
+function vehicles() {
+
+  function convertImgToBase64(url, callback, outputFormat){
+    var canvas = document.createElement('CANVAS'),
+        ctx = canvas.getContext('2d'),
+        img = new Image;
+    img.crossOrigin = 'Anonymous';
+    img.onload = function(){
+        canvas.height = img.height;
+        canvas.width = img.width;
+        ctx.drawImage(img,0,0);
+        var dataURL = canvas.toDataURL(outputFormat || 'image/jpg');
+        callback.call(this, dataURL);
+        canvas = null; 
+    };
+    img.src = url;
+  }
+
+  function onPicSuccess(imageURI){
+    convertImgToBase64(imageURI, function(base64){
+      var file = new Parse.File("photo.jpg", { base64: base64 });
+
+      if (file == null)
+      {
+        showMessage('Invalid picture, please try again');
+      }
+
+      file.save().then(function() {
+        var field = [{field: "photo", value: file}];
+        var successFun = function(vehicle) {
+          showMessage("Picture changed");
+          $('#vehicle-image').attr('src',imageURI); 
+        }
+        var errorFun = function(vehicle, error) {
+          showMessage("Picture could not be uploaded:\n"+error.message);
+          console.log(error.message); 
+        }
+        Ridekeeper.user.updateVehicle(pageVehicleId, field, successFun, errorFun);
+
+
+      }, function(error)  {
+        onPicFail(error);
+        return;
+      });
+    });
+  }
+
+  function onPicFail(message){
+    showMessage('Failed to set picture: ' + message);
+  }
+  
+  function newGalleryPicture() {
+  navigator.camera.getPicture(onPicSuccess, onPicFail, { quality: 50,
+    destinationType: Camera.DestinationType.FILE_URI,
+    allowEdit: true,
+    targetWidth: 200,
+    targetHeight: 200,
+    sourceType: Camera.PictureSourceType.PHOTOLIBRARY,
+    saveToPhotoAlbum: false
+    });
+  }
+
+  function newCameraPicture() {
+  navigator.camera.getPicture(onPicSuccess, onPicFail, { quality: 50,
+    destinationType: Camera.DestinationType.FILE_URI,
+    allowEdit: true,
+    targetWidth: 200,
+    targetHeight: 200,
+    sourceType: Camera.PictureSourceType.CAMERA,
+    saveToPhotoAlbum: false
+    });
+  }
+
+  function sourceSelected( buttonIndex ) {
+    if (buttonIndex == 1)
+      newCameraPicture();
+
+    if (buttonIndex == 2)
+      newGalleryPicture();
+    // Do nothing, the user exited the dialog
+  }
+
+  this.newPicture = function() {
+      // Prompt for gallery or camera as source
+        navigator.notification.confirm('Select a source for the new image', sourceSelected , 'Image Source', ["Camera","Gallery"]);
+  }
 }
